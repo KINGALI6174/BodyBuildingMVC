5774a24ac96686bd7f9bf9f4c5bc8bc9f707cb72
Ali Aboli <98606709+KINGALI6174@users.noreply.github.com>
2023-11-07 10:39:30 +0330

    CreateDb

---
+++ BodyBuilding.Domain/Entities/Athlete/Athlete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BodyBuilding.Domain.Entities.Athlete
+{
+    public class Athlete
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string NationalCode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ BodyBuilding.Infrastructuer/Migrations/20231107070339_initialContextAndCreateTableAthleteMovment.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BodyBuilding.Infrastructuer.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialContextAndCreateTableAthleteMovment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Athletes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Family = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NationalCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Athletes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movements",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Set = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movements", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Athletes");
+
+            migrationBuilder.DropTable(
+                name: "Movements");
+        }
+    }
+}
