b0a500cb06b02f300893b0c1f704d7ac6003224a
Ali Aboli <98606709+KINGALI6174@users.noreply.github.com>
2023-11-22 17:37:10 +0330

    add DTO And Add Identity

--- BodyBuilding.Infrastructuer/DbContext/BodyBuildingDbContext.cs
+++ BodyBuilding.Infrastructuer/DbContext/BodyBuildingDbContext.cs
@@ -5,13 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BodyBuilding.Domain.Entities.Movement;
-
-;
+using BodyBuilding.Domain.Entities.Athlete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace BodyBuilding.Infrastructuer.DbContext
 {
-    public class BodyBuildingDbContext : Microsoft.EntityFrameworkCore.DbContext
+    public class BodyBuildingDbContext : IdentityDbContext<IdentityUser>
     {
+        public BodyBuildingDbContext(DbContextOptions<BodyBuildingDbContext> options) :base(options)
+        {
+
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source =DESKTOP-K81JHQ4;Initial Catalog =BodyBuilding;Integrated Security=True;multipleActiveResultSets=True;TrustServerCertificate=True");
---
+++ BodyBuilding/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using BodyBuilding.Application.DTOs;
+using BodyBuilding.Domain.Entities.Athlete;
+using BodyBuilding.Infrastructuer.DbContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BodyBuilding.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Ctro
+
+        private readonly BodyBuildingDbContext _Context;
+
+        public AccountController(BodyBuildingDbContext context)
+        {
+            _Context = context;
+        }
+
+        #endregion
+
+        #region register
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Register(AthleteRegisterDTO athleteDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                Athlete athlete = new Athlete()
+                {
+                    Name = athleteDTO.Name,
+                    Family = athleteDTO.Family,
+                    BirthDay = athleteDTO.BirthDay,
+                    Gender = athleteDTO.Gender,
+                    NationalCode = athleteDTO.NationalCode,
+                    PhoneNumber = athleteDTO.PhoneNumber,
+                    Password = athleteDTO.Password,
+                };
+                _Context.Athletes.Add(athlete);
+                _Context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        #endregion
+
+        #region login
+
+
+
+        #endregion
+
+        #region logout
+
+
+
+        #endregion
+
+    }
+}
